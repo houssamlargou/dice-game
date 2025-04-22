160031583197257f210cd2eb79347bf589d94a02
Largou <houssamlargou9@gmail.com>
2025-04-22 13:17:34 +0100

    feat(game): implement game initialization and reset on new game click

--- script.js
+++ script.js
@@ -13,14 +13,27 @@ const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add("hidden");
+let scores, currentScore, activePlayer, play;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let play = true;
+const initialization = () => {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  play = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add("hidden");
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
+};
+
+initialization();
 
 const switchPlayer = () => {
   currentScore = 0;
@@ -74,3 +87,5 @@ document.querySelector(".btn--hold").addEventListener("click", () => {
     }
   }
 });
+
+btnNew.addEventListener("click", initialization);
