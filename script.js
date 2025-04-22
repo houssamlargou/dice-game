f6d78d9a418e182b82c09630a4e26707d5959ff5
Largou <houssamlargou9@gmail.com>
2025-04-22 04:06:16 +0100

    init(game): add basic player switching logic

---
+++ script.js
@@ -0,0 +1,50 @@
+"use strict";
+
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
+
+const score0El = document.querySelector("#score--0");
+const score1El = document.getElementById("score--1");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
+
+const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add("hidden");
+
+const score = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
+btnRoll.addEventListener("click", () => {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  diceEl.classList.remove("hidden");
+  diceEl.src = `dice/dice-${dice}.png`;
+  if (dice !== 1) {
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    currentScore = 0;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+
+    if (activePlayer == 0) {
+      activePlayer = 1;
+    } else {
+      activePlayer = 0;
+    }
+
+    player0El.classList.toggle("player--active");
+    player1El.classList.toggle("player--active");
+  }
+});
+
+document.querySelector(".btn--hold").addEventListener("click", () => {
+  console.log("houssam");
+});
